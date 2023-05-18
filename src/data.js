14bfd72d0a15b871b18eaaeefc73123496c579b4
front-ed <edwinschutjes@gmail.com>
2023-05-18 15:09:48 +0200

    update: data

--- src/data.js
+++ src/data.js
@@ -1,16 +1,4 @@
-export const shortList = [
-  {
-    id: 1,
-    image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
-    name: "maria ferguson",
-    title: "office manager",
-    quote:
-      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
-  },
-];
-
-export const list = [
+export const testList = [
   {
     id: 1,
     image:
